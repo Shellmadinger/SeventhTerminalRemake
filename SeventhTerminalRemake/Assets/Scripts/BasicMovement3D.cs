4643d88e0f0540fbb14a918556ba7b2b37ed2861
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2024-05-30 12:15:45 -0400

    Switch Player to CharacterController
    Replaced the Rigidbody on the Player in favor of a character controller.

--- SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
+++ SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
@@ -6,9 +6,12 @@ public class BasicMovement3D : MonoBehaviour
 {
     public float speed;
     public GameManager currentGameState;
-    public Rigidbody body;
+    public float gravityFactor;
+    //public Rigidbody body;
+    public CharacterController playerCon;
     float horiMove;
     float vertMove;
+    float gravity;
     Vector3 fullMovement;
 
     // Update is called once per frame
@@ -22,13 +25,18 @@ public class BasicMovement3D : MonoBehaviour
         //Check if the current game state is 1, which is when actual gameplay should start
         if(currentGameState.gameState == 1)
         {
+            gravity -= gravityFactor * Time.deltaTime;
             //Get x and Y axises
             horiMove = Input.GetAxis("Horizontal");
             vertMove = Input.GetAxis("Vertical");
             fullMovement = new Vector3(horiMove, 0f, vertMove);
             //Move Gameobject
             Vector3 move = transform.TransformDirection(fullMovement) * speed;
-            body.velocity = new Vector3(move.x, body.velocity.y, move.z);
+            playerCon.Move(new Vector3(move.x,gravity,move.z));
+            if (playerCon.isGrounded)
+            {
+                gravity = 0;
+            }
            
         }
        
