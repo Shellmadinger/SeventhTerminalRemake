44452c48d43b15a41917715a576688b9e7b24b81
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2024-05-31 12:23:25 -0400

    Reverted Switch to CharacterController
    TLDR, the character controller wasn't working out as I absolutely need the rigidbody to do what I need to do. Fortunately, I think the new input system might help here

--- SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
+++ SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
@@ -7,11 +7,11 @@ public class BasicMovement3D : MonoBehaviour
     public float speed;
     public GameManager currentGameState;
     public float gravityFactor;
-    //public Rigidbody body;
-    public CharacterController playerCon;
+    public Rigidbody body;
+    //public CharacterController playerCon;
     float horiMove;
     float vertMove;
-    float gravity;
+    //float gravity;
     Vector3 fullMovement;
 
     // Update is called once per frame
@@ -25,18 +25,14 @@ public class BasicMovement3D : MonoBehaviour
         //Check if the current game state is 1, which is when actual gameplay should start
         if(currentGameState.gameState == 1)
         {
-            gravity -= gravityFactor * Time.deltaTime;
+            //gravity -= gravityFactor * Time.deltaTime;
             //Get x and Y axises
             horiMove = Input.GetAxis("Horizontal");
             vertMove = Input.GetAxis("Vertical");
             fullMovement = new Vector3(horiMove, 0f, vertMove);
             //Move Gameobject
             Vector3 move = transform.TransformDirection(fullMovement) * speed;
-            playerCon.Move(new Vector3(move.x,gravity,move.z));
-            if (playerCon.isGrounded)
-            {
-                gravity = 0;
-            }
+            body.velocity = new Vector3(move.x, body.velocity.y, move.z);
            
         }
        
