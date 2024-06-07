df4c1db3c61fcf293ebaad8527fef9c2ef774213
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2024-06-07 11:53:26 -0400

    Adjusted Player Movement
    Create gravity and isGrounded check for player. Just about everything regarding movement is manually controlled through variables

--- SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
+++ SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
@@ -8,16 +8,17 @@ public class BasicMovement3D : MonoBehaviour
     public GameManager currentGameState;
     public float gravityFactor;
     public Rigidbody body;
-    //public CharacterController playerCon;
     float horiMove;
     float vertMove;
-    //float gravity;
+    float gravity;
     Vector3 fullMovement;
+    bool isGrounded;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Move();
+        Debug.Log(gravity);
     }
 
     void Move()
@@ -25,17 +26,36 @@ public class BasicMovement3D : MonoBehaviour
         //Check if the current game state is 1, which is when actual gameplay should start
         if(currentGameState.gameState == 1)
         {
-            //gravity -= gravityFactor * Time.deltaTime;
+            //reduce gravity by gravity factor
+            gravity -= gravityFactor * Time.deltaTime;
             //Get x and Y axises
             horiMove = Input.GetAxis("Horizontal");
             vertMove = Input.GetAxis("Vertical");
             fullMovement = new Vector3(horiMove, 0f, vertMove);
             //Move Gameobject
             Vector3 move = transform.TransformDirection(fullMovement) * speed;
-            body.velocity = new Vector3(move.x, body.velocity.y, move.z);
+            body.velocity = new Vector3(move.x, gravity, move.z);
+            //Make gravity zero is grounded
+            if(isGrounded == true) { gravity = 0; }
            
         }
        
+    }
+
+    //Collision checks for isGrounded
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground") {isGrounded = true;}
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground") { isGrounded = false;}
     }
 
 }
