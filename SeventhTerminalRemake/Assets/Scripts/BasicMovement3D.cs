a782db499fb371572c86b599066601c5c5b2264b
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2024-06-27 12:58:10 -0400

    Adjusted Movement (Again)
    No but really, all I did was stop trying to use my own gravity and just mess with the physics gravity (that and make gravity heavier on the ground but lighter when the player jumps). Some refinement is needed but this is the best movement I made so far.

--- SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
+++ SeventhTerminalRemake/Assets/Scripts/BasicMovement3D.cs
@@ -6,7 +6,8 @@ public class BasicMovement3D : MonoBehaviour
 {
     public float speed;
     public GameManager currentGameState;
-    public float gravityFactor;
+    public float gravityOnGround;
+    public float gravityOnJump;
     public Rigidbody body;
     [SerializeField] KnockBack knockBackBool;
     float horiMove;
@@ -15,13 +16,11 @@ public class BasicMovement3D : MonoBehaviour
     float gravity;
     Vector3 fullMovement;
     bool isGrounded;
-
+    bool isJumping;
     // Update is called once per frame
     void FixedUpdate()
     {
         Move();
-
-        
     }
 
     void Move()
@@ -29,24 +28,29 @@ public class BasicMovement3D : MonoBehaviour
         //Check if the current game state is 1, which is when actual gameplay should start
         if (currentGameState.gameState == 1)
         {
-            //reduce gravity by gravity factor
-            gravity -= gravityFactor * Time.deltaTime;
-            //Make gravity zero is grounded
-            if (isGrounded == true) { gravity = 0; }
+            if(isGrounded == true) { Physics.gravity = new Vector3(0,(gravityOnGround * -1),0); }
+            if(isJumping == true && isGrounded == false) { Physics.gravity = new Vector3(0, -9.81f, 0); }
             //Get x and Y axises
             horiMove = Input.GetAxis("Horizontal");
             vertMove = Input.GetAxis("Vertical");
             fullMovement = new Vector3(horiMove, 0f, vertMove);
             //Move Gameobject
             Vector3 move = transform.TransformDirection(fullMovement) * speed;
-            body.velocity = new Vector3(move.x, gravity, move.z);
+            body.velocity = new Vector3(move.x, body.velocity.y, move.z);
 
             if (Input.GetButton("Jump") && isGrounded == true)
             {
-                //Vector3 jumpForce = Vector3.up * 100f;
-                body.velocity = body.velocity + Vector3.up * (gravity - Physics.gravity.y) * 1000 * Time.deltaTime;
-                Debug.Log(body.velocity);
+                isJumping = true;
+                if (isJumping == true)
+                {
+                    //Vector3 jumpForce = Vector3.up * 100f;
+                    body.velocity += (Vector3.up * Physics.gravity.y * (gravityOnJump - 1) * Time.deltaTime) * -1;
+                    Debug.Log(body.velocity);
+                }
+                
             }
+
+            Debug.Log(Physics.gravity);
         }
 
     }
@@ -54,12 +58,13 @@ public class BasicMovement3D : MonoBehaviour
     //Collision checks for isGrounded
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground") { isGrounded = true; }
+        if (collision.gameObject.tag == "Ground") { isGrounded = true; isJumping = false; }
     }
 
     private void OnCollisionStay(Collision collision)
     {
         isGrounded = true;
+        isJumping = false;
     }
 
     private void OnCollisionExit(Collision collision)
